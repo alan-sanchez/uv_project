8d573464e249f17f78c36258da2bdaac1e83cbb0
Alan G. Sanchez <sanchala@oregonstate.edu>
2023-04-25 11:16:01 -0700

    Fixed naming of variables.

--- src/transform_accumulation_merge.py
+++ src/transform_accumulation_merge.py
@@ -45,8 +45,8 @@ class TransformPCL(object):
         ## Initialize self.command
         self.command = "stop"
 
-        ## Initialize OcTree function with a resolution of 0.05 meters
-        self.resolution = rospy.get_param('resolution')#0.05
+        ## Initialize OcTree function with a resolution of 0.01 meters
+        self.resolution = rospy.get_param('resolution')#0.01
         self.octree = octomap.OcTree(self.resolution)
 
         self.measuring_ratio = (0.01/self.resolution)**2 # 0.01 is the side length of UV sensor
@@ -59,7 +59,7 @@ class TransformPCL(object):
         self.bound = 0.17 
 
         ## The required UV Dose for a UV rate constant of 0.0867 m^2/J
-        ## at 99% disinfection rate is, 53.10 (J/m^2)
+        ## at 99.9% disinfection rate is, 151.68 (J/m^2)
         self.required_dose = 151.66 #53.10 #151.68
 
         ## Command that begins and stops accumulation map and markers
@@ -95,7 +95,7 @@ class TransformPCL(object):
         self.markerArray = MarkerArray()
 
         ## Create sensor array region 
-        self.region = [[0.72, 0.55], [0.74, 0.55], [0.74, -0.55], [0.72, -0.55]]    # Sensor Array
+        self.region = [[0.72, 0.55], [0.74, 0.55], [0.74, -0.55], [0.72, -0.55]]   # Sensor Array
         #self.region = [[0.70, 0.07], [0.90, 0.07], [0.90, -.113], [0.70, -.113]]  # Cone
         #self.region = [[0.75, 0.05], [0.90, 0.05], [0.90, -0.09], [0.75, -0.09]]  # Mug
         self.line = geometry.LineString(self.region)
@@ -106,8 +106,7 @@ class TransformPCL(object):
 
     def callback_oct_center_pcl2(self, pcl2_msg):
         """
-        A function that stores the PointCloud2 message types which represents 
-        the center of the cells from the octomap.
+        A function that stores the PointCloud2 message. 
         :param self: The self reference.
         :param pcl2_msg: The PointCloud2 message.
         """
@@ -116,8 +115,7 @@ class TransformPCL(object):
 
     def callback_command(self,str_msg):
         """
-        A function that publishes the octomap center cells as a PointCloud message type.
-        The function also transforms the coordinates to reference the `base_link`. 
+        A function that transforms a PointCloud to reference the `base_link`. 
         :param self: The self reference.
         :param str_msg: A String message type.
         """
@@ -163,6 +161,7 @@ class TransformPCL(object):
                 w.writerow([key[0],key[1],key[2],val])
                 # print([key[0],key[1],key[2],val])
 
+
     def callback_combined_pcl2(self, pcl2_msg):
         """
         Callback function that stores the PointCloud2 message of the combined
@@ -184,112 +183,102 @@ class TransformPCL(object):
                 pcl_cloud.points.append(Point32(data[0],data[1],data[2]))
 
             ## Transform the pointcloud message to reference the `base_link`
-            base_cloud = self.transform_pointcloud(pcl_cloud, "/base_link")
+            baselink_pcl = self.transform_pointcloud(pcl_cloud, "/base_link")
 
             ## Transform the pointcloud message to reference the `uv_light_link`
-            uv_light_cloud = self.transform_pointcloud(pcl_cloud,"/uv_light_link")
+            uv_light_pcl = self.transform_pointcloud(pcl_cloud, "/uv_light_link")
 
-            self.accumulator(uv_light_cloud,base_cloud)
+            ## This resets the prev_time variable to None after a trajectory execution
+            ## is complete. This is because there is a 2 second wait time before the user
+            ## can command another trajectory
+            if (rospy.get_time() - self.prev_time) > 2.0:
+                self.prev_time = rospy.get_time()
+                self.time_exposure = 0
 
+            ## Create temporary empty lists and execute for loop to temporary uv dose map
+            temp_dict = dict()
 
-    def accumulator(self, uv_light_pcl, baselink_pcl):
-        """
-        Callback function that transforms gripper camera to reference the `base_link` 
-        and `uv_light_link`.
-        :param self: The self reference.
-        :param uv_light_pcl: A PointCloud message referencing the uv_light_link.
-        :param baselink_pcl: A PointCloud message referencing the base_link.
-        """
-        ## This resets the prev_time variable to None after a trajectory execution
-        ## is complete. This is because there is a 2 second wait time before the user
-        ## can command another trajectory
-        if (rospy.get_time() - self.prev_time) > 2.0:
-            self.prev_time = rospy.get_time()
-            self.time_exposure = 0
+            for uv_light_coord, base_coord in zip(uv_light_pcl.points, baselink_pcl.points):
+                ## Check if the base_coord is in the defined region    
+                point = geometry.Point(base_coord.x,base_coord.y)
+                if self.polygon.contains(point) == False:
+                    continue
 
-        ## Create temporary empty lists and execute for loop to temporary uv dose map
-        temp_dict = dict()
+                ## Calculate the angle (radians) between the z-axis vector and 
+                ## uv flashlight point coordinates, `uv_light_coord`
+                ray_length = np.linalg.norm([uv_light_coord.x,uv_light_coord.y,uv_light_coord.z])        
+                numerator = np.dot(self.grip_vec, [uv_light_coord.x,uv_light_coord.y,uv_light_coord.z])
+                denominator = self.mag_grip_vec * ray_length
+                rad = np.arccos(numerator/denominator)
 
-        for uv_light_coord, base_coord in zip(uv_light_pcl.points, baselink_pcl.points):
-            ## Check if the base_coord is in the defined region    
-            point = geometry.Point(base_coord.x,base_coord.y)
-            if self.polygon.contains(point) == False:
-                continue
-
-            ## Calculate the angle (radians) between the z-axis vector and 
-            ## uv flashlight point coordinates, `uv_light_coord`
-            ray_length = np.linalg.norm([uv_light_coord.x,uv_light_coord.y,uv_light_coord.z])        
-            numerator = np.dot(self.grip_vec, [uv_light_coord.x,uv_light_coord.y,uv_light_coord.z])
-            denominator = self.mag_grip_vec * ray_length
-            rad = np.arccos(numerator/denominator)
-
-            # print(rad)
-            # print(ray_length)
-           
-            if rad < self.bound:
-                chk, key = self.octree.coordToKeyChecked(np.array([base_coord.x, base_coord.y, base_coord.z]))
-                # print(chk)
-                if chk:
-                    ## compute the UV dose for the conical `rad` value
-                    radius = 0.3 * math.tan(rad)
-                    ir = self.eqn_model(radius) * 10 # multiply by 10 to convert from mW/cm^2 to W/m^2
-                    dist_ratio = (0.3**2)/(ray_length**2) # Inverse sqaure law ratio
-                    dose = dist_ratio * self.time_exposure * ir 
-
-                    ## Pull coordinates of cell key
-                    pos = tuple(self.octree.keyToCoord(key))
-
-                    if self.item == True:
-                        if pos[2] < 0.75:
-                            continue
-                    
-                    if pos in temp_dict:
-                        temp_dict[pos].append(dose)     
-                    else:
-                        temp_dict[pos] = [dose,]
-
-        ## Create marker array of cells
-        for pose_key in temp_dict:
-            # dose_2 = sum(temp_dict[pose_key])*self.measuring_ratio
-            # dose_2 = sum(temp_dict[pose_key])/len(temp_dict[pose_key])
-            dose_value = max(temp_dict[pose_key])
-            # hits = len(temp_dict[pose_key])
-
-            # print(dose_value, dose_2, hits)
-            ## Update accumulation map dictionary
-            if pose_key in self.acc_map_dict:
-                self.acc_map_dict[pose_key] += dose_value
-            else:
-                self.acc_map_dict[pose_key] = dose_value
-
-            ## Update cube id dictionary
-            if pose_key in self.cube_id_dict:
-                self.marker.id = self.cube_id_dict[pose_key]
-            else:
-                self.cube_id_dict[pose_key] = len(self.cube_id_dict)
-
-                self.marker.id = self.cube_id_dict[pose_key]
+                # print(rad)
+                # print(ray_length)
             
-            ## Update the color id based on UV values
-            if self.acc_map_dict[pose_key] < 0.25 * self.required_dose:
-                self.marker.color = ColorRGBA(1,   0,  0, 0.5)
-            elif self.acc_map_dict[pose_key] < 0.5 * self.required_dose:
-                self.marker.color = ColorRGBA(1, 0.5,  0, 0.65)
-            elif self.acc_map_dict[pose_key] < 0.75 * self.required_dose:
-                self.marker.color = ColorRGBA(1, 1,  0, 0.85)
-            else:
-                self.marker.color = ColorRGBA(  0, 1,  0, 1)
+                if rad < self.bound:
+                    chk, key = self.octree.coordToKeyChecked(np.array([base_coord.x, base_coord.y, base_coord.z]))
+                    # print(chk)
+                    if chk:
+                        ## compute the UV dose for the conical `rad` value
+                        radius = 0.3 * math.tan(rad)
+                        ir = self.eqn_model(radius) * 10 # multiply by 10 to convert from mW/cm^2 to W/m^2
+                        dist_ratio = (0.3**2)/(ray_length**2) # Inverse sqaure law ratio
+                        dose = dist_ratio * self.time_exposure * ir 
 
-            ## Set point to marker
-            self.marker.points = [Point(pose_key[0], pose_key[1], pose_key[2]),]
+                        ## Pull coordinates of cell key
+                        pos = tuple(self.octree.keyToCoord(key))
 
-            ## Append and publish 
-            self.markerArray.markers.append(self.marker)
-            self.MarkerArray_publisher.publish(self.markerArray)
+                        if self.item == True:
+                            if pos[2] < 0.75:
+                                continue
+                        
+                        if pos in temp_dict:
+                            temp_dict[pos].append(dose)     
+                        else:
+                            temp_dict[pos] = [dose,]
 
-        ## Set new prev_time as current time before the beginning of next loop iteration
-        self.time_exposure = rospy.get_time() - self.prev_time
-        self.prev_time = rospy.get_time()
+            ## Create marker array of cells
+            for pose_key in temp_dict:
+                # dose_2 = sum(temp_dict[pose_key])*self.measuring_ratio
+                # dose_2 = sum(temp_dict[pose_key])/len(temp_dict[pose_key])
+                dose_value = max(temp_dict[pose_key])
+                # hits = len(temp_dict[pose_key])
+
+                # print(dose_value, dose_2, hits)
+                ## Update accumulation map dictionary
+                if pose_key in self.acc_map_dict:
+                    self.acc_map_dict[pose_key] += dose_value
+                else:
+                    self.acc_map_dict[pose_key] = dose_value
+
+                ## Update cube id dictionary
+                if pose_key in self.cube_id_dict:
+                    self.marker.id = self.cube_id_dict[pose_key]
+                else:
+                    self.cube_id_dict[pose_key] = len(self.cube_id_dict)
+
+                    self.marker.id = self.cube_id_dict[pose_key]
+                
+                ## Update the color id based on UV values
+                if self.acc_map_dict[pose_key] < 0.25 * self.required_dose:
+                    self.marker.color = ColorRGBA(1,   0,  0, 0.5)
+                elif self.acc_map_dict[pose_key] < 0.5 * self.required_dose:
+                    self.marker.color = ColorRGBA(1, 0.5,  0, 0.65)
+                elif self.acc_map_dict[pose_key] < 0.75 * self.required_dose:
+                    self.marker.color = ColorRGBA(1, 1,  0, 0.85)
+                else:
+                    self.marker.color = ColorRGBA(  0, 1,  0, 1)
+
+                ## Set point to marker
+                self.marker.points = [Point(pose_key[0], pose_key[1], pose_key[2]),]
+
+                ## Append and publish 
+                self.markerArray.markers.append(self.marker)
+                self.MarkerArray_publisher.publish(self.markerArray)
+
+            ## Set new prev_time as current time before the beginning of next loop iteration
+            self.time_exposure = rospy.get_time() - self.prev_time
+            self.prev_time = rospy.get_time()
+
 
     def transform_pointcloud(self,pcl_cloud, target_frame):
         """
