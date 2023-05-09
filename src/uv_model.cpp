2e034e98acc03054fad6b6677ef6329cb7596750
Alan G. Sanchez <sanchala@oregonstate.edu>
2023-05-08 18:49:24 -0700

    Changed source file to a class format

--- src/uv_model.cpp
+++ src/uv_model.cpp
@@ -5,29 +5,33 @@ a given distance, x, value
 
 #include "uv_project/uv_model.h"
 
-using namespace std;
+/*
+Constructor of the `uv_model` class used to define variables and constants
+*/
+uv_model::uv_model(){
+    coefficients = {26.96, -19.75, 88.01, -175.6, 170.9, -96.81, 
+                    35.18, -8.703, 1.517, -0.19, 0.01718, -0.001112,
+                    5.032e-05, -1.511e-6, 2.705e-08, -2.186e-10};
+    ir_value = 0;
+    power_x = 1;
+}
+
 
 /*
 This function computes the irradiance from the uv model
+@param x: double 
+
+@returns ir_value: double
 */
-double model(double x)
-{
-    // Coefficients of the 15th order polynomial 
-    vector<double> coefficients  {26.96, -19.75, 88.01, -175.6, 170.9, -96.81, 
-                                  35.18, -8.703, 1.517, -0.19, 0.01718, -0.001112,
-                                  5.032e-05, -1.511e-6, 2.705e-08, -2.186e-10};
-
-    // Declare irradiance value and power order
-    double ir_value = 0,power_x = 1;
-
-    // For loop to compute irradiance value  
+double uv_model::model(double x){
+    // // For loop to compute irradiance value  
     for (int i = 0; i < coefficients.size(); i++)
     {
         ir_value += coefficients[i] * power_x;
         power_x *= x;
     }
     
-    // return irradiance value. Units are (mW/cm^2)
+    // // return irradiance value. Units are (mW/cm^2)
     return ir_value; 
 }
 
