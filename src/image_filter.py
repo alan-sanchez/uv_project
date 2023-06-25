65aa3f7f9a7d1d33e5586e37c098c4f8fb84a609
Alan Sanchez <sanchala@oregonstate.edu>
2023-06-25 16:21:20 -0700

    Updated image filter bounds.

--- src/image_filter.py
+++ src/image_filter.py
@@ -36,7 +36,7 @@ class ImageFilter():
 
         ## set the lower and upper bounds for the desired hue
         # self.lower_col = np.array([0, 20, 170]) # Table, older camera
-        self.lower_col = np.array([0, 40, 180]) # Table
+        self.lower_col = np.array([0, 5, 150]) # Table
         #self.lower_col = np.array([0, 5, 240]) # Cone
         # self.lower_col = np.array([0, 20, 100]) # Mug
         self.upper_col = np.array([180, 255, 255])
