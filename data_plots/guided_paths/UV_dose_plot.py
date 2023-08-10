ea58c1b85ae869eb1aca21d88f6a9648248f952d
Alan Sanchez <sanchala@oregonstate.edu>
2023-08-09 21:38:28 -0700

    Updated data and scripts

---
+++ data_plots/guided_paths/UV_dose_plot.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot_hist(values, bins=10):
+    # Create the histogram with custom bins
+    # plt.hist(values,bins=bins, edgecolor='black')
+
+    # Add a vertical line at a specific value (e.g., x=0)
+    plt.axvline(x=1, color='red', linestyle='--', label='Required UV dose')
+
+    # Create the histogram with centered bins
+    n, bins, patches = plt.hist(values, bins=bins, edgecolor='black')
+
+    # Calculate the width of each bin
+    bin_width = bins[1] - bins[0]
+
+    # Calculate the bin centers
+    bin_centers = bins[:-1] + bin_width / 2
+
+    num_inactive_cells = 0
+
+    # Add labels to the centers of the bins
+    for count, x in zip(n, bin_centers):
+        plt.text(x, count, str(int(count)), ha='center', va='bottom')
+        if x >.9:
+            num_inactive_cells += count
+    
+    # labels = [f"{b}" if b != 2.25 else "2.5+" for b in bins]
+    plt.xlabel('Percentage of the required UV dose')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Hard Coded Path (# of Inactivated/Green Cells: '+str(int(num_inactive_cells))+')')
+    # plt.xticks(bins, labels)
+    plt.grid(axis='y')
+    plt.legend()
+    plt.show()
+
+
+## Read the CSV file into a pandas DataFrame
+df = pd.read_csv('FB_cone_guided_2.csv')
+
+## Extract the UV_dose coordinates
+values = df['UV_dose']/156.11
+
+# ## Set any value over 2.0 to 2.0 using NumPy's where function
+# values = np.where(values > 2.0, 2.125, values)
+
+## 
+# custom_bins = [0, .25, .50, .75, 1, 1.25, 1.5, 1.75, 2.0, +2.25]
+
+##
+plot_hist(values)#, custom_bins)
+
---
+++ data_plots/recorded_paths/3D_plot.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot_cube(ax, x, y, z, color):
+    ## Define cube dimensions
+    dx = dy = dz = 0.01
+    
+    ## Define cube position
+    x -= dx / 2
+    y -= dy / 2
+    z -= dz / 2
+    
+    ## Plot the cube
+    ax.bar3d(x, y, z, dx, dy, dz, color=color)
+
+## Read the CSV file into a pandas DataFrame
+df = pd.read_csv('cone_data.csv')
+
+## Extract the x, y, and z coordinates
+x = df['X_pos']
+y = df['Y_pos']
+z = df['Z_pos']
+color = df['UV_dose']
+
+## Define the desired range for the color mapping
+color_min = 0.0
+color_max = 151.66
+
+## Define a colormap with reversed order
+cmap = plt.cm.get_cmap('RdYlGn')
+
+## Create the figure and axis
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+## Plot the cubes with colors and custom color range
+for xi, yi, zi, color_val in zip(x, y, z, color):
+    cube_color = cmap((color_val - color_min) / (color_max - color_min))
+    plot_cube(ax, xi, yi, zi, cube_color)
+
+## Plot the points
+# scatter = ax.scatter(x, y, z, c=color, cmap=cmap, vmin=color_min, vmax=color_max)
+
+## Set plot labels
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Add title to the graph
+plt.title('Hard Coded Path')
+
+## Set equal spacing for the grid lines
+ax.set_box_aspect([.3,.5,.9])
+
+## Add a color bar
+# cbar = fig.colorbar(scatter)
+cbar = fig.colorbar(plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=color_min, vmax=color_max)))
+cbar.set_label('UV_dose (W/m^2)')
+
+
+
+## Show the plot
+plt.show()
